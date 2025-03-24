158660777ea11c9801eabdbdf0f3a872b5bc644b
rotarymars <s.goto2050@gmail.com>
2025-03-24 17:54:53 +0900

    added serial print

--- src/main.cpp
+++ src/main.cpp
@@ -9,11 +9,13 @@ void setup() {
 }
 
 void loop() {
+  Serial.println("HIGH");
   analogWrite(26,255);
   analogWrite(27,255);
   analogWrite(31,255);
   analogWrite(32,255);
   sleep(1000);
+  Serial.println("LOW");
   analogWrite(26,0);
   analogWrite(27,0);
   analogWrite(31,0);
