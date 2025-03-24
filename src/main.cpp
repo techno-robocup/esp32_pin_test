7d6022ace71b9be2c3f69ecbd5daa3c98caf968a
rotarymars <s.goto2050@gmail.com>
2025-03-24 17:53:52 +0900

    initialzie commit!

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+#include <Arduino.h>
+
+
+void setup() {
+  pinMode(26,OUTPUT);
+  pinMode(27,OUTPUT);
+  pinMode(31,OUTPUT);
+  pinMode(32,OUTPUT);
+}
+
+void loop() {
+  analogWrite(26,255);
+  analogWrite(27,255);
+  analogWrite(31,255);
+  analogWrite(32,255);
+  sleep(1000);
+  analogWrite(26,0);
+  analogWrite(27,0);
+  analogWrite(31,0);
+  analogWrite(32,0);
+  sleep(1000);
+}
